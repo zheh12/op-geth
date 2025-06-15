585c05e0fb007295346e8051b877bd06d9d93133
yang <zheh12@gmail.com>
2025-06-15 08:52:27 +0000

    add more logs

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -372,6 +372,7 @@ func prepare(ctx *cli.Context) {
 // It creates a default node based on the command line arguments and runs it in
 // blocking mode, waiting for it to be shut down.
 func geth(ctx *cli.Context) error {
+	log.Info("MyLog: use MyLog image")
 	if args := ctx.Args().Slice(); len(args) > 0 {
 		return fmt.Errorf("invalid command: %q", args[0])
 	}
